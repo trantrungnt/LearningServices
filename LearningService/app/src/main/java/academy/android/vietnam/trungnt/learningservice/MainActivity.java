ad41395544aa438aefe9f1aa3fd6c568064e15fb
Trần Trung <nguyentrantrung1986@gmail.com>
2015-12-29 11:49:44 -0800

    demo Service (status: Started)

--- LearningService/app/src/main/java/academy/android/vietnam/trungnt/learningservice/MainActivity.java
+++ LearningService/app/src/main/java/academy/android/vietnam/trungnt/learningservice/MainActivity.java
@@ -2,6 +2,8 @@ package academy.android.vietnam.trungnt.learningservice;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void startService(View view){
+        startService(new Intent(getBaseContext(), MyService.class));
+    }
+
+    public void stopService(View view) {
+        stopService(new Intent(getBaseContext(), MyService.class));
     }
 }
